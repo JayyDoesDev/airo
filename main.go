c142db8195b16bbdd193f08381cd902cd9bd548a
Tarab1te <69683068+JayyDoesDev@users.noreply.github.com>
2025-07-08 22:47:36 -0400

    Update main.go

--- main.go
+++ main.go
@@ -1,13 +1,9 @@
 package main
 
 import (
-	//	"fmt"
 	"log"
 	"os"
 
-	//	"os"
-
-	//	"github.com/jayydoesdev/airo/bot/lib"
 	"github.com/jayydoesdev/airo/bot/discord"
 	"github.com/joho/godotenv"
 )
@@ -19,16 +15,4 @@ func main() {
 	}
 
 	discord.StartAiro(os.Getenv("DISCORD_BOT_TOKEN"))
-	/*	client, err := lib.NewClient("openai", os.Getenv("OPENAI_API_KEY"))
-		if err != nil {
-			panic(err)
-		}
-
-		res, err := client.Send("hello")
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		fmt.Println("AI says:", res)
-		fmt.Println("Hello World") */
 }
