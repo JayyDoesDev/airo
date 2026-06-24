52297384b130e67f40eac8ea547b13c2aaf1280c
Tarab1te <69683068+JayyDoesDev@users.noreply.github.com>
2026-06-24 14:44:12 -0400

    Update messageCreate.go

--- discord/events/messageCreate.go
+++ discord/events/messageCreate.go
@@ -58,7 +58,7 @@ func HandleGoogleSearch(opts HandleGoogleSearchOpts) (string, bool, []lib.Refere
 	if len(items) == 0 {
 		newResp, err = opts.Client.Send(
 			opts.Message.Author.ID, opts.Message.Author.Username, *opts.Guild,
-			opts.FullPrompt+"lib.SecondPromptResultsNotFound",
+			opts.FullPrompt+lib.SecondPromptResultsNotFound,
 			opts.Memory,
 		)
 	} else {
@@ -190,13 +190,15 @@ func OnMessageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 			promptMem += fmt.Sprintf("- [Long] %s: %s\n", item.Title, item.Content)
 		}
 
-		fullPrompt := promptMem + "\nUser says: " + content
-
 		guild, err := s.Guild(m.GuildID)
 		if err != nil {
 			s.ChannelMessageSend(m.ChannelID, "Error: "+err.Error())
 			return
 		}
+
+		botPerms := getBotPermissions(s, guild, m.ChannelID)
+		fullPrompt := promptMem + "\nYour permissions in this server:\n" + formatPermissions(botPerms) + "\n\nUser says: " + content
+
 		resp, err := client.Send(m.Author.ID, m.Author.Username, *guild, fullPrompt, mem)
 		if err != nil {
 			s.ChannelMessageSend(m.ChannelID, "Error: "+err.Error())
@@ -354,4 +356,106 @@ func MakeExecute(task lib.Action, s *discordgo.Session, m *discordgo.MessageCrea
 	return func() error {
 		return lib.HandleActions(task, s, m)
 	}
+}
+
+func getBotPermissions(s *discordgo.Session, guild *discordgo.Guild, channelID string) int64 {
+	botID := s.State.User.ID
+
+	member, err := s.GuildMember(guild.ID, botID)
+	if err != nil {
+		return 0
+	}
+
+	var perms int64
+	for _, role := range guild.Roles {
+		if role.ID == guild.ID {
+			perms |= role.Permissions
+			break
+		}
+	}
+
+	for _, roleID := range member.Roles {
+		for _, role := range guild.Roles {
+			if role.ID == roleID {
+				perms |= role.Permissions
+				break
+			}
+		}
+	}
+
+	if perms&discordgo.PermissionAdministrator != 0 {
+		return discordgo.PermissionAll
+	}
+
+	channel, err := s.State.Channel(channelID)
+	if err != nil {
+		return perms
+	}
+
+	for _, ow := range channel.PermissionOverwrites {
+		if ow.ID == guild.ID {
+			perms &^= ow.Deny
+			perms |= ow.Allow
+			break
+		}
+	}
+
+	var allow, deny int64
+	for _, roleID := range member.Roles {
+		for _, ow := range channel.PermissionOverwrites {
+			if ow.ID == roleID {
+				allow |= ow.Allow
+				deny |= ow.Deny
+			}
+		}
+	}
+	perms &^= deny
+	perms |= allow
+
+	for _, ow := range channel.PermissionOverwrites {
+		if ow.ID == botID {
+			perms &^= ow.Deny
+			perms |= ow.Allow
+			break
+		}
+	}
+
+	return perms
+}
+
+func formatPermissions(perms int64) string {
+	type perm struct {
+		bit  int64
+		name string
+	}
+	all := []perm{
+		{discordgo.PermissionAdministrator, "Administrator"},
+		{discordgo.PermissionManageServer, "Manage Server"},
+		{discordgo.PermissionManageRoles, "Manage Roles"},
+		{discordgo.PermissionManageChannels, "Manage Channels"},
+		{discordgo.PermissionKickMembers, "Kick Members"},
+		{discordgo.PermissionBanMembers, "Ban Members"},
+		{discordgo.PermissionManageMessages, "Manage Messages"},
+		{discordgo.PermissionMentionEveryone, "Mention Everyone"},
+		{discordgo.PermissionModerateMembers, "Timeout Members"},
+		{discordgo.PermissionSendMessages, "Send Messages"},
+		{discordgo.PermissionReadMessageHistory, "Read Message History"},
+		{discordgo.PermissionViewChannel, "View Channels"},
+		{discordgo.PermissionEmbedLinks, "Embed Links"},
+		{discordgo.PermissionAttachFiles, "Attach Files"},
+		{discordgo.PermissionAddReactions, "Add Reactions"},
+		{discordgo.PermissionManageNicknames, "Manage Nicknames"},
+		{discordgo.PermissionChangeNickname, "Change Nickname"},
+	}
+
+	var granted []string
+	for _, p := range all {
+		if perms&p.bit != 0 {
+			granted = append(granted, "- "+p.name)
+		}
+	}
+	if len(granted) == 0 {
+		return "- None"
+	}
+	return strings.Join(granted, "\n")
 }
